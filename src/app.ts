691a7f9d4af372be95a4018e457d124797d7ea32
VictrCruz312 <motoggb979@gmail.com>
2023-01-30 13:12:20 -0300

    feat: criando pastas padrões: controllers, services, routes e adicionando as routes no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import contactsRouter from "./routes/contacts.routes";
+import userRouter from "./routes/user.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("/contacts", contactsRouter);
 
 export default app;
---
+++ src/routes/contacts.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const contactsRouter = Router();
+
+contactsRouter.post("");
+contactsRouter.get("");
+contactsRouter.patch("/:id");
+contactsRouter.delete("/:id");
+
+export default contactsRouter;
