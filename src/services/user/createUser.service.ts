b178ba0a48512939f45f3bc39cb8dc4a9d57480e
VictrCruz312 <motoggb979@gmail.com>
2023-01-31 15:16:20 -0300

    feat: adicionando o service de create user que faltou no último commit

---
+++ src/services/user/createUser.service.ts
@@ -0,0 +1,36 @@
+import { hashSync } from "bcrypt";
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/AppError";
+import { createUserSchema } from "../../schemas/user.schema";
+
+const createUserService = async (data: User) => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const userValidated = await createUserSchema
+    .validate(data, { abortEarly: false })
+    .catch((error) => {
+      throw new AppError("error", 400, error.errors);
+    });
+  console.log(userValidated);
+  const emailAlreadyExists = await userRepository.findOneBy({
+    email: userValidated.email,
+  });
+
+  if (emailAlreadyExists) {
+    throw new AppError("Email já existe", 400);
+  }
+  const user = userRepository.create({
+    email: userValidated.email,
+    name: userValidated.name,
+    password: hashSync(userValidated.password, 10),
+    cellphone: userValidated.cellphone,
+    telephone: userValidated.telephone ? userValidated.telephone : undefined,
+  });
+  await userRepository.save(user);
+
+  const { password, ...userNotPassword } = user;
+  return userNotPassword;
+};
+
+export default createUserService;
