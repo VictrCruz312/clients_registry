13e056c160d8d90a5f7b768e0e753333c5703cc3
VictrCruz312 <motoggb979@gmail.com>
2023-02-01 13:29:51 -0300

    fix: removendo consoles.log

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -2,8 +2,6 @@ import { Request, Response } from "express";
 import createAdminService from "../services/admin/createAdmin.service";
 
 const createAdminController = async (req: Request, res: Response) => {
-  console.log(req.body);
-  const {} = req.body;
   const admin = await createAdminService(req.body, req.body.userAdmin.admin);
 
   return res.status(201).json(admin);
